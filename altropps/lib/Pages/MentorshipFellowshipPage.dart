cf6ec912e1c0c954d5eb8268b6416e563754867a
ADIBA NOOR <adiba.noor003@gmail.com>
2022-12-12 03:24:26 +0530

    displayed fetched data

--- altropps/lib/Pages/MentorshipFellowshipPage.dart
+++ altropps/lib/Pages/MentorshipFellowshipPage.dart
@@ -2,6 +2,7 @@ import 'package:altropps/Pages/HomePage.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MentorshipFellowshipPage extends StatefulWidget {
   const MentorshipFellowshipPage({Key? key}) : super(key: key);
@@ -12,9 +13,17 @@ class MentorshipFellowshipPage extends StatefulWidget {
 
 class _MentorshipFellowshipPageState extends State<MentorshipFellowshipPage> {
   final ref = FirebaseDatabase.instance.ref('FELLOWSHIPS');
+  // launchURL(String url) async {
+  //   if(await canLaunchUrl(Uri url)){
+  //     await launch(url , forceWebView: true);
+  //   }else{
+  //     throw 'Could not Launch $url';
+  //   }
+  // }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         leading: InkWell(
             onTap: (){
@@ -31,8 +40,25 @@ class _MentorshipFellowshipPageState extends State<MentorshipFellowshipPage> {
               child: FirebaseAnimatedList(
                 query: ref,
                 itemBuilder: (context, snapshot, animation, index) {
-                  return ListTile(
-                    title: Text(snapshot.child('B').value.toString()),
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8,horizontal: 20),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color:Color(0xFFD58DE0) ,
+                        borderRadius: BorderRadius.circular(20)
+                      ),
+                      child: ListTile(
+                        leading: Icon(Icons.person,color: Colors.white,),
+                        title: Text(snapshot.child('A').value.toString()),
+                        subtitle: Text(snapshot.child('B').value.toString()),
+                        tileColor: Color(0xFFD58DE0),
+                        onTap: (){
+                          // const url = Uri.parse("snapshot.child('B').value.toString()")
+                        },
+                        minVerticalPadding: 10,
+                        contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+                      ),
+                    ),
                   );
                 }
               ),
