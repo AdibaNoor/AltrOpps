7ad7891d4fe6597fc5f429d2a8f0420f5dbac0af
SanskritiHarmukh <sanskritiharmukh1908@gmail.com>
2023-01-05 17:57:10 +0530

    Added Internship data

--- altropps/lib/Pages/OpenSourcePage.dart
+++ altropps/lib/Pages/OpenSourcePage.dart
@@ -13,7 +13,7 @@ class OpenSourcePage extends StatefulWidget {
 }
 
 class _OpenSourcePageState extends State<OpenSourcePage> {
-  final ref = FirebaseDatabase.instance.ref('Sheet1');
+  final ref = FirebaseDatabase.instance.ref('Fellowship&Mentorship');
   @override
   Widget build(BuildContext context) {
     double w = MediaQuery.of(context).size.width;
